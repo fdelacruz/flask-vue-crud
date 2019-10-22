d291ca4615970e192d790f710d2bd08066ad3534
Francisco De La Cruz <helloquico@gmail.com>
2019-10-22 07:06:24 -0400

    Use a unique identifier

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import uuid
 
 DEBUG = True
 
@@ -12,16 +13,19 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 BOOKS = [
     {
+        'id': uuid.uuid4().hex,
         'title': 'On the Road',
         'author': 'Jack Kerouac',
         'read': True
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Harry Potter and the Philosopher\'s Stone',
         'author': 'J. K. Rowling',
         'read': False
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Green Eggs and Ham',
         'author': 'Dr. Seuss',
         'read': True
