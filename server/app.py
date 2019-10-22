564f66e9ef9cbb56fdcb061851a5cbb16e298e3e
Francisco De La Cruz <helloquico@gmail.com>
2019-10-22 07:13:22 -0400

    Add a PUT route to update a book

--- server/app.py
+++ server/app.py
@@ -33,7 +33,6 @@ BOOKS = [
 ]
 
 
-<<<<<<< HEAD
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
@@ -54,6 +53,22 @@ def all_books():
         response_object['message'] = 'Book added!'
     else:
         response_object['books'] = BOOKS
+    return jsonify(response_object)
+
+
+@app.route('/books/<book_id>', methods=['PUT'])
+def single_book(selfbook_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book updated!'
     return jsonify(response_object)
 
 
